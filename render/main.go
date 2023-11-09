37e681dc4559d82a11cd75cd305320e4c8c86fe2
hexiaodai <jianmin.he@daocloud.io>
2023-11-09 15:01:49 +0800

    update index.html

---
+++ render/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"text/template"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+var blogs map[interface{}]interface{}
+
+const (
+	templateFile = "template"
+	blogsFile    = "blogs.yaml"
+	htmlFile     = "../index.html"
+)
+
+func main() {
+	templateByte, err := os.ReadFile(templateFile)
+	if err != nil {
+		panic(err)
+	}
+	tmpl, err := template.New("index").Funcs(funcMap()).Parse(string(templateByte))
+	if err != nil {
+		panic(err)
+	}
+
+	var buf bytes.Buffer
+	if err := tmpl.Execute(&buf, blogs); err != nil {
+		panic(err)
+	}
+
+	f, _ := os.Create(htmlFile)
+	defer f.Close()
+	if _, err := buf.WriteTo(f); err != nil {
+		panic(err)
+	}
+}
+
+func init() {
+	data, err := os.ReadFile(blogsFile)
+	if err != nil {
+		panic(err)
+	}
+	if err := yaml.Unmarshal(data, &blogs); err != nil {
+		panic(err)
+	}
+}
+
+func funcMap() template.FuncMap {
+	return template.FuncMap{
+		"inc": func(i int) int {
+			return i + 1
+		},
+	}
+}
